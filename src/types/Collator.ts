77324801d20b0b5a2db006ef629c0a087aad0d8f
rob thijssen <rthijssen@gmail.com>
2022-12-15 16:54:24 +0200

    collator rename (#327)
    Signed-off-by: rob thijssen <rthijssen@gmail.com>
    
    Signed-off-by: rob thijssen <rthijssen@gmail.com>

--- src/types/Collator.ts
+++ src/types/Collator.ts
@@ -13,7 +13,7 @@ const ADDRESS_TO_COLLATOR_NAME_MAP = {
   dmuuG83f3JeXBmMp7e3XssJzq7rUAuNgAT3z7HoUPWueqpD1V: 'lh',
   dmy6WPM2KfD7WBxJYS6UG17GHJVCv8kewiwTr6ciVeXLbBpvf: 'CrypTech',
   dmvFayQJ9S7BgbHE2kmnoVq9UfdbfwHpZ4d1revJfVA6X9dGR: 'SeaFoodShop',
-  dmu7rmwTa35Ec5cnNMpn8EpnFPA727sDtpCQwu9uCo2sfnmg1: '🇭🇺',
+  dmu7rmwTa35Ec5cnNMpn8EpnFPA727sDtpCQwu9uCo2sfnmg1: 'черно море',
   dmvuGKcNe4VEv1rBVcTFEavAsccciEXcWoEi5iQrdR1NNMD1w: 'orange skies',
   dmx4WhyUDhAjsMf1mRD55qApjxnqSXcSsmweHgcv8seGkrN4R: 'staker-space',
   dmyZopEVaerkgSWWTd4WScPkhQgHTeLfMcHVCkQUyL1gu29c3: 'mini rocket',
